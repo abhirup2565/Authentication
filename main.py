11f418d2435189644d3a093015f6b1697a6df85e
abhirup2565 <abhirup2565@gmail.com>
2025-07-26 15:20:59 +0530

    jwt error handler

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 from extensions import db,jwt
 from dotenv import load_dotenv
 from auth import auth_blueprint
@@ -9,12 +9,32 @@ def create_app():
     app=Flask(__name__)
     #config file
     app.config.from_prefixed_env()
+
+
     #initialising extentions
     db.init_app(app)
     jwt.init_app(app)
+
+
     #intialising blueprint
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(user_blueprint)
+
+
+    #jwt error handler
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header,jwt_data):
+        return jsonify({"message":"Token is expired","error":"Token expired"}),401
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({"message":"Signature verification failed","error":"invalid token"}),401
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({"message":"Request does not contain valid token","error":"authorization_header"}),401
+
+
     return app
 
 
--- users.py
+++ users.py
@@ -1,10 +1,12 @@
 from flask import Blueprint,request,jsonify
+from flask_jwt_extended import jwt_required
 from models import User
 from schemas import userSchema
 
 user_blueprint=Blueprint('users',__name__)
 
 @user_blueprint.route("/all",methods=["GET"])
+@jwt_required()
 def get_all_users():
     page=request.args.get('page',default=1,type=int)
     per_page=request.args.get('per_page',default=3,type=int)
