67e926bf8044eb39f30750e3ca3d6637bbc942d6
abhirup2565 <abhirup2565@gmail.com>
2025-07-26 10:43:24 +0530

    sqlachemy setup

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from extensions import db
+from dotenv import load_dotenv
+load_dotenv()
 
 def create_app():
     app=Flask(__name__)
     app.config.from_prefixed_env()
+    db.__init__(app)
     return app
 
 
 app=create_app()
 
 if __name__=="__main__":
+    with app.app_context():
+        create_app()
     app.run()
